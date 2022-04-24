7056ddf337f909b14bf669d9551db5cca0600a91
ahmed fathi <ahmedfathi520@gmail.com>
2022-04-24 17:27:48 +0200

    Add project files.

---
+++ C sharp toutrial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // The using System line means that you are using the System library in your project
+using System.Collections.Generic;
+
+namespace HelloWorld // namespaces are used to logically arrange classes
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
