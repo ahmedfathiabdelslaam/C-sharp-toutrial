46943844b30ebd56a258c4f0af8b292dbe45e7b2
ahmed fathi <ahmedfathi520@gmail.com>
2022-04-25 05:24:04 +0200

    finishing c# toutrial

--- C sharp toutrial/Program.cs
+++ C sharp toutrial/Program.cs
@@ -1,13 +1,46 @@
 ﻿using System; // The using System line means that you are using the System library in your project
 using System.Collections.Generic;
 
-namespace HelloWorld // namespaces are used to logically arrange classes
+namespace variables // namespaces are used to logically arrange classes
 {
     class Program
     {
         static void Main (string[] args)
         {
-            Console.WriteLine("Hello World");
+            /*
+            var number = 2;
+            var count = 10;
+            var total_price = 20.9f;
+            var charcter = 'A';
+            var firstname = "ahmed fathi";
+            var isworking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(total_price);
+            Console.WriteLine(charcter);
+            Console.WriteLine(firstname);
+            Console.WriteLine(isworking);
+            */
+            // Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            // Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            ///const float pi = 3.14f;
+            //pi = 17;
+            //int i = 1;
+            //byte b = (byte) i;
+            try
+            {
+                var number = "1234";
+                //int i = Convert.ToInt32 (number);
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("canot convert from string to byte");
+            }
+
         }
     }
 }
